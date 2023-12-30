e1a44a4867d83f5c89ea3c8481a05384cf51ebdc
Manojkumar <manojprfp@gmail.com>
2023-12-30 18:28:27 +0530

    added apollo client

--- pages/SportNews/index.js
+++ pages/SportNews/index.js
@@ -1,12 +1,37 @@
 import Navbar from "@/components/Navbar";
+import { useQuery, gql } from "@apollo/client";
+// import CountriesList from "@/components/CountriesList";
 
+function SportNews() {
+    const Get_countries = gql`
+  query Countries {
+    countries {
+      code
+      name
+      emoji
+    }
+  }
+`;
+  const { data, loading, error } = useQuery(Get_countries);
+  if (loading) {
+    return (
+      <h2>
+        Loading...
+      </h2>
+    );
+  }
+  if (error) {
+    console.error(error);
+    return null;
+  }
 
-function SportNews(){
-    return(
-        <div className="container">
-            <Navbar/>
-            <h1>SportNews</h1>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h1 style={{textAlign: 'center'}}>Hyderabad NewsPaper</h1>
+      <Navbar />
+      <h1>SportNews</h1>
+      {/* <CountriesList data={data.countries}/> */}
+    </div>
+  );
 }
 export default SportNews;
